446fb20224561ea066bfbd92614a4faf6cda18b7
simonk <s.kopciewski@dlamnie.net>
2022-11-25 11:15:18 +0100

    Update account_chooser

--- data/mutt/account_chooser.sh
+++ data/mutt/account_chooser.sh
@@ -7,7 +7,7 @@ ACCOUNT_DIR="${CURRENT_DIR}/accounts"
 
 main() {
   local profile_file=$1; shift
-  ADDR=$(grep -e "^Delivered-To:" -e "^To:" -f /dev/stdin | sed -e "s/^Delivered-To:\s//" -e "s/^To:\s//" | grep -Eio '\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b')
+  ADDR=$(grep -e "^Delivered-To:" -e "^To:" | sed -e "s/^Delivered-To:\s//" -e "s/^To:\s//" | grep -Eio '\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b')
   for email in $ADDR; do
     account_file="${ACCOUNT_DIR}/${email}"
     if [ -f "$account_file" ]; then
